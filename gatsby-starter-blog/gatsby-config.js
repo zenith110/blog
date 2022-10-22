7db67b81ca8324241ad21eed47bb93f25590268c
zenith110 <thequake@live.com>
2022-10-22 10:55:27 -0400

    Modified bio

--- gatsby-starter-blog/gatsby-config.js
+++ gatsby-starter-blog/gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `Abrahan's Blog`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Abrahan Nevarez`,
+      summary: ``,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://gatsbystarterblogsource.gatsbyjs.io/`,
